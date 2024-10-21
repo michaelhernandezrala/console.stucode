4347e1bde494fe4f511ae819151cfb50f8074912
Michael Hernandez <michaelhernandezrala@gmail.com>
2024-10-21 15:03:05 +0200

    chore: setup project

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import globals from "globals";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.jsx"],
+    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+  },
+  pluginReactConfig,
+  {
+    rules: {
+      "no-unused-vars": ["error"],
+      "react/react-in-jsx-scope": "off",
+      "max-len": ["error", { code: 120 }],
+    },
+  },
+];
